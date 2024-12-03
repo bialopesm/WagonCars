c350e71fdd56273ba9bd6e2832e4784c390daaa7
Nominoe Girard <nominoe.girard@gmail.com>
2024-12-03 11:59:50 -0300

    validations & link for each car

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,3 +1,9 @@
 class Car < ApplicationRecord
   belongs_to :user
+
+  validates :model, :year, :category, :location, :quality, :color, :picture, presence: true
+  validates :year, numericality: {
+    greater_than_or_equal_to: 1900,
+    less_than_or_equal_to: -> { Date.current.year }
+  }
 end
