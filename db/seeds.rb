b5200b6a6ef1c3c78504bf163cbfe98134ffeb0c
Beatriz Machado <bialmachado@gmail.com>
2024-12-05 12:53:41 -0300

    Navbar fixed

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,5 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+login = User.new(user_name: 'bialopesm', email: 'usuario@exemple.com', password: 123456)
+login.save!
